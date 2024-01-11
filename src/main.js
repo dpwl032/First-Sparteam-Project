e1bf26eb5c3c12b29f2bef14399477c5d4637a62
chomusuke9072 <chomusuke9072@gmail.com>
2024-01-11 19:40:19 +0900

    모달창 적용

--- src/main.js
+++ src/main.js
@@ -15,5 +15,34 @@ form.addEventListener("submit", (event) => {
 });
 
 
+//card-list 누르면 모달창 켜지게 하기
+const modal = document.getElementById("modal")
+const btnModal = document.getElementById("card-list")
+btnModal.addEventListener("click", e => {
+    modal.style.display = "flex"
+})
+
+//모달창의 클로즈(x) 버튼을 누르면 모달창이 꺼지게 하기
+const closeBtn = modal.querySelector(".close-area")
+closeBtn.addEventListener("click", e => {
+    modal.style.display = "none"
+})
+
+
+//모달창 바깥 영역을 클릭하면 모달창이 꺼지게 하기
+modal.addEventListener("click", e => {
+  const evTarget = e.target
+  if(evTarget.classList.contains("modal-overlay")) {
+      modal.style.display = "none"
+  }
+})
+
+//모달창이 켜진 상태에서 ESC 버튼을 누르면 모달창이 꺼지게 하기
+window.addEventListener("keyup", e => {
+  if(modal.style.display === "flex" && e.key === "Escape") {
+      modal.style.display = "none"
+  }
+})
+
 
 
