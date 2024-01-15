2f7ad369ea41523af6d5a21d6fd348dc7a964ae3
서가희 <kahee430@gmail.com>
2024-01-15 17:40:05 +0900

    댓글 기능 구현 완료

--- src/comment.js
+++ src/comment.js
@@ -35,7 +35,7 @@ clickedMoviev.appendChild(movieOverview);
 clickedMoviev.appendChild(movieVoteAverage);
 
 
-// 댓글기능 구현
+// =========== 댓글기능 구현 =========== 
 
 // 댓글 입력 폼 
 const commentForm = document.querySelector('.commentForm');
@@ -63,16 +63,11 @@ commentForm.addEventListener("submit", (e) => {
     pw.focus();
   };
 
-  // 닉네임 중복 안됨, 단 닉네임당 댓글 작성도 하나만 할 수 있다
-  // if (localStorage.getItem(userName) !== null) {
-  //   alert('동일한 이름이 존재합니다. 다른 이름을 사용해주세요.');
-  //   return;
-  // }
 
   // 로컬스토리지에 저장된 영화 아이디값을 기준으로 댓글 불러오기
   let getComment = window.localStorage.getItem(clickedMovieId);
 
-  // 문자열로 변환된 댓글을 다시 객체로 변환 
+  // 로컬스토리지는 문자열로 저장된다, 문자열로 변환된 댓글을 다시 객체로 변환 
   // 문자열은 그대로 "userName" 이렇게 값이 나오기 때문 우리가 원하는 값은 userName 변수에 들어있는 값이다(EX.userName : 김뽀삐)
   let returnObjcomment = JSON.parse(getComment);
 
@@ -83,9 +78,8 @@ commentForm.addEventListener("submit", (e) => {
   console.log("returnObjcomment", returnObjcomment);
   console.log("getComment", getComment);
 
-
   // 댓글 객체 생성(작성자, 댓글 내용, 비밀번호) 
-  const id = Date.now();
+  const id = Date.now(); // 댓글 삭제를 위한 id값
   const makeComment = {
     userName: userName.value,
     comment: comment.value,
@@ -100,7 +94,7 @@ commentForm.addEventListener("submit", (e) => {
   // JSON.stringify 메서드로 객체를 문자열로 변환
   const objComment = JSON.stringify(returnObjcomment);
 
-  // 댓글 객체를 문자열로 변환 후 로컬스토리지에 저장
+  // 댓글 객체 영화id를 키값으로 로컬스토리지에 저장
   const setComment = localStorage.setItem(clickedMovieId, objComment);
   console.log(clickedMovieId);
 
@@ -110,6 +104,7 @@ commentForm.addEventListener("submit", (e) => {
   const userText = document.createElement('p');
   const deletBtn = document.createElement('button');
 
+  // 댓글 삭제 버튼 클레스 네임 부여
   deletBtn.className = 'deletBtn';
 
   // 댓글 객체 쪼개기(닉넴, 내용을 하나씩 요소에 담기)
@@ -123,6 +118,7 @@ commentForm.addEventListener("submit", (e) => {
   commentBox.append(comments);
   comments.append(deletBtn);
 
+  // 댓글 목록 불러오는 함수 호출
   loadComments();
 });
 
@@ -132,13 +128,13 @@ loadComments();
 
 // 댓글 불러오기
 function loadComments() {
-  // 모든 댓글을 가져오기
+  // 모든 댓글을 영화id 키값으로 가져오기
   const allComments = JSON.parse(localStorage.getItem(clickedMovieId));
   console.log("allComments", allComments);
 
-  // 가져온 댓글을 화면에 표시
   commentBox.innerHTML = ''; // 기존의 댓글 목록 초기화
 
+  // 댓글 목록 화면에 뿌리기
   allComments.forEach(comment => {
     const commentElement = document.createElement('li');
     commentElement.innerText = `${comment.comment} - ${comment.userName}`;
@@ -154,19 +150,20 @@ function loadComments() {
 }
 
 
-// 비밀번호 확인 삭제
+// 비밀번호 삭제
 commentBox.addEventListener("click", (e) => {
 
+  // 이벤트 대상의 클레스가 deleteBtn이면 작동
   if (e.target.classList.contains('deleteBtn')) {
 
-    // 삭제하려는 댓글 
-    const commentElement = e.target.parentElement;
-    const id = commentElement.dataset.id;
+    const commentElement = e.target.parentElement; // 이벤트 대상(삭제 대상)의 부모 요소를 변수에 담음
+    const id = commentElement.dataset.id; // 이벤트 대상(삭제 대상)의 id값을 변수에 담음
     // console.log("id", commentElement.dataset.id)
 
     // 댓글 가져오기
     const comments = JSON.parse(localStorage.getItem(clickedMovieId));
 
+    // 댓글 id와 이벤트 대상의 id가 같다는 조건을 반환
     const comment = comments.find(function (comment) {
       return String(comment.id) === id
     });
@@ -175,19 +172,21 @@ commentBox.addEventListener("click", (e) => {
     // 비밀번호 입력 받기
     const userPassword = prompt("댓글을 삭제하려면 비밀번호를 입력하세요:");
 
-    // // 입력된 비밀번호와 저장된 비밀번호 비교
+    // // 입력된 비밀번호와 댓글에 저장된 비밀번호 비교
     if (userPassword === comment.pw) {
 
       // 비밀번호가 일치하는 경우 댓글 삭제
       alert('테스트');
 
-      commentElement.remove();  // html에서 요소 삭제
-      
+      commentElement.remove();  // html에서 댓글 요소 삭제
+
+      // 필터를 통해 댓글의 id와 이벤트 대상의 id가 같지 않은것만 반환한다(id값이 같은 대상은 삭제)
       let filteredComments = comments.filter(function (comment) {
         return String(comment.id) !== id
       });
       // console.log("filteredComments",filteredComments)
 
+      // 필터로 이벤트 대상이 아닌 id값만 모인 댓글은 로컬스토리지에 다시 저장해준다.
       localStorage.setItem(clickedMovieId, JSON.stringify(filteredComments));
 
       alert("댓글이 삭제되었습니다.");
